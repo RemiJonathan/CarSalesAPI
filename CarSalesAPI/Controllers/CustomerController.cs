7ae821273f4d3e54ced261b6934d788a7e53fcc6
Dylan <dylanpita007@users.noreply.github.com>
2019-11-19 09:35:25 -0500

    Fixed Routes

--- CarSalesAPI/Controllers/CustomerController.cs
+++ CarSalesAPI/Controllers/CustomerController.cs
@@ -17,26 +17,26 @@ namespace CarSalesAPI.Controllers
         }
 
         // GET api/<controller>/5
-        [Route("api2/Customer/GetCustomer/{id?}")]
+        [Route("api2/GetCustomer/{id?}")]
         public string Get(int id)
         {
             return "value";
         }
 
         // POST api/<controller>
-        [Route("api2/Customer/AddCustomer")]
+        [Route("api2/AddCustomer")]
         public void Post([FromBody]string value)
         {
         }
 
         // PUT api/<controller>/5
-        [Route("api2/Customer/UpdateCustomer/{id?}")]
+        [Route("api2/UpdateCustomer/{id?}")]
         public void Put(int id, [FromBody]string value)
         {
         }
 
         // DELETE api/<controller>/5
-        [Route("api2/Customer/DeleteCustomer/{id?}")]
+        [Route("api2/DeleteCustomer/{id?}")]
         public void Delete(int id)
         {
         }
