55ad0a6aa94b65be6201cb8443510855de89f956
Peter H <pgwh96@outlook.com>
2019-11-26 12:14:26 -0500

    wrote the stuff for the sales person controller

--- CarSalesAPI/Controllers/SalespersonController.cs
+++ CarSalesAPI/Controllers/SalespersonController.cs
@@ -10,35 +10,128 @@ namespace CarSalesAPI.Controllers
     public class SalespersonController : ApiController
     {
         // GET api/<controller>
-        [Route("api2/GetSalespersons")]
-        public IEnumerable<string> Get()
+        [Route("api2/Salespersons")]
+        [HttpGet]
+        public HttpResponseMessage Get()
         {
-            return new string[] { "value7", "value8" };
+            try
+            {
+                List<Salesperson> entities = db.Salesperson.ToList();
+
+                if (entities == null)
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound,
+                        "No Sales People Found.");
+                }
+
+                List<ApiSalesperson> apiSalesperson = new List<ApiSalesperson>();
+
+                foreach (var record in entities)
+                {
+                    ApiSalesperson apiSalesperson = new ApiSalesperson();
+                    PropertyCopier<Salesperson, ApiSalesperson>.Copy(record, apiSalesperson);
+                    apiSalesperson.Add(apiSalesPerson);
+                }
+                return Request.CreateResponse(HttpStatusCode.OK, apiSalesperson);
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest,
+                        "Error in the code");
+            }
         }
 
         // GET api/<controller>/5
-        [Route("api2/GetSalesperson/{id?}")]
-        public string Get(int id)
+        [Route("api2/Salesperson/{id?}")]
+        [HttpGet]
+        public HttpResponseMessage Get(int id)
         {
-            return "value";
+            try
+            {
+                var entity = db.Salesperson.Find(id);
+
+                if (entity == null)
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound,
+                        "Sales Person with Id " + id.ToString() + " not found.");
+                }
+
+                var myApiSaleperson = new ApiSalesperson();
+
+                PropertyCopier<Salesperson, ApiSalesperson>.Copy(entity, myApiSalesperson);
+
+                return Request.CreateResponse(HttpStatusCode.OK, myApiSalesperson);
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest,
+                         "Error in the code");
+            }
         }
 
         // POST api/<controller>
-        [Route("api2/AddSalesperson")]
-        public void Post([FromBody]string value)
+        [Route("api2/Salesperson")]
+        [HttpPost]
+        public HttpResponseMessage Post([FromBody]ApiSalesperson newSalesperson)
         {
+            try
+            {
+                Salesperson s = new Salesperson();
+                PropertyCopier<ApiSaleperson, Salesperson>.Copy(newSalesperson, s);
+                db.Salesperson.Add(s);
+                db.SaveChanges();
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound,
+                    "Cannot add new Sales Person, Try again.");
+            }
         }
 
         // PUT api/<controller>/5
-        [Route("api2/UpdateSalesperson/{id?}")]
-        public void Put(int id, [FromBody]string value)
+        [Route("api2/Salesperson/{id?}")]
+        [HttpPut]
+        public HttpResponseMessage Put(int id, [FromBody]ApiSalesperson newSalesperson)
         {
+            try
+            {
+                var entity = db.Salesperson.FirstOrDefault(x => x.SalespersonId == id);
+
+                if (entity == null)
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound,
+                        "Sales Person with Id " + id.ToString() + " not found to update.");
+                }
+
+                PropertyCopier<ApiSalesperson, Salesperson>.Copy(newSalesperson, entity);
+                db.SaveChanges();
+
+                return Request.CreateResponse(HttpStatusCode.OK,
+                    "Sales Person with Id " + id.ToString() + " found and updated.");
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest,
+                        "Error in the code");
+            }
         }
 
         // DELETE api/<controller>/5
-        [Route("api2/DeleteSalesperson/{id?}")]
-        public void Delete(int id)
+        [Route("api2/Salesperson/{id?}")]
+        public HttpResponseMessage(int id)
         {
+            try
+            {
+                db.Salesperson.Find(id).SalespersonIsActive = false;
+                db.SaveChanges();
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest,
+                        "Error in the code");
+            }
         }
     }
 }
